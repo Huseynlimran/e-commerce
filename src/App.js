89afc35d97c03e45aa86ab2dc08378bd4f572e7e
Huseynlimran <huseynlimran@gmail.com>
2022-11-16 20:39:20 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Nav from "./components/Nav";
+import Navbar from "./components/Navbar";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 
 function App() {
-
-
   return (
     <div className="App">
       <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route>
-          <Route path="/" element={<Home />}></Route>
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<Home />}></Route>
 
-          <Route path="/cart" element={<Cart />}></Route>
-
-        </Route>
-      </Routes>
+            <Route path="/cart" element={<Cart />}></Route>
+        </Routes>
       </BrowserRouter>
     </div>
   );
