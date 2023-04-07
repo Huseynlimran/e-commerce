2d2b85d73e652f740e0ffb940a59ed1b3a875b54
Imran <huseynlimran@gmail.com>
2023-04-07 14:56:17 +0400

    data

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className='home'>
         <h1>Welcome to webstore</h1>
-        <img src='img/9c28de0e-b9e7-4a3f-be05-715d5a2c770a.webp' ></img>
+        <img src='img/9c28de0e-b9e7-4a3f-be05-715d5a2c770a.webp' alt='bg'></img>
     </div>
   )
 }
